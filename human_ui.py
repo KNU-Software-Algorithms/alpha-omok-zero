372816c6166114677a571bafe61ff0f76242020b
Jungdae Kim <kekmodel@gmail.com>
2018-05-11 15:05:42 +0900

    First commit

---
+++ human_ui.py
@@ -0,0 +1,92 @@
+from __future__ import print_function
+from omok_env import OmokEnv
+from mcts_uct import MCTS
+import numpy as np
+
+N, Q = 0, 1
+CURRENT = 0
+OPPONENT = 1
+COLOR = 2
+BLACK = 1
+WHITE = 0
+BOARD_SIZE = 9
+HISTORY = 8
+COLUMN = {"a": 0, "b": 1, "c": 2,
+          "d": 3, "e": 4, "f": 5,
+          "g": 6, "h": 7, "i": 8,
+          "j": 9, "k": 10, "l": 11,
+          "m": 12, "n": 13, "o": 14,
+          "A": 0, "B": 1, "C": 2,
+          "D": 3, "E": 4, "F": 5,
+          "G": 6, "H": 7, "I": 8,
+          "J": 9, "K": 10, "L": 11,
+          "M": 12, "N": 13, "O": 14}
+THINK_TIME = 600
+GAME = 1
+
+
+class HumanAgent:
+    def get_action(self):
+        laskt_str = str(BOARD_SIZE)
+        for k, v in COLUMN.items():
+            if v == BOARD_SIZE - 1:
+                laskt_str += k
+                break
+        move_target = str(input('1a ~ {}: '.format(laskt_str)))
+        row = int(move_target[:1]) - 1
+        col = COLUMN[move_target[1:2]]
+        action = row * BOARD_SIZE + col
+        return action
+
+
+class HumanUI:
+    def __init__(self):
+        self.human = HumanAgent()
+        self.ai = MCTS(BOARD_SIZE, HISTORY, THINK_TIME)
+
+    def get_action(self, state, board, idx):
+        if idx % 2 == 0:
+            action = self.human.get_action()
+        else:
+            action = self.ai.get_action(state, board)
+        return action
+
+
+def play():
+    env = OmokEnv(BOARD_SIZE, HISTORY)
+    manager = HumanUI()
+    result = {-1: 0, 0: 0, 1: 0}
+    for g in range(GAME):
+        print('##########   Game: {}   ##########'.format(g + 1))
+        state, board = env.reset()
+        done = False
+        idx = 0
+        while not done:
+            env.render()
+            # start simulations
+            action = manager.get_action(state, board, idx)
+            state, board, z, done = env.step(action)
+            idx += 1
+        if done:
+            if z == 1:
+                result['Black'] += 1
+            elif z == -1:
+                result['White'] += 1
+            else:
+                result['Draw'] += 1
+            # render & reset tree
+            env.render()
+            manager.ai.reset_tree()
+        # result
+        print('')
+        print('=' * 20, " {}  Game End  ".format(g + 1), '=' * 20)
+        blw, whw, drw = result['Black'], result['White'], result['Draw']
+        stat = (
+            'Black Win: {}  White Win: {}  Draw: {}  Winrate: {:0.1f}%'.format(
+                blw, whw, drw,
+                1 / (1 + np.exp(whw / (g + 1)) / np.exp(blw / (g + 1))) * 100))
+        print(stat, '\n')
+
+
+if __name__ == '__main__':
+    play()
---
+++ mcts_zero.py
@@ -0,0 +1,307 @@
+from __future__ import print_function
+from omok_env import OmokEnv
+import time
+import sys
+from collections import deque, defaultdict
+import numpy as np
+from numpy import random, sqrt, argwhere, zeros, ones, exp
+import torch
+import torch.optim as optim
+from torch.nn import functional as F
+from torch.autograd import Variable
+from torch.utils.data import DataLoader
+from neural_net import PVNet
+
+N, Q, P = 0, 1, 2
+CURRENT = 0
+OPPONENT = 1
+COLOR = 2
+BLACK = 1
+WHITE = 0
+N_BLOCK = 10
+CHANNEL = 128
+BOARD_SIZE = 9
+HISTORY = 2
+N_SIMUL = 400
+N_GAME = 1
+N_EPOCH = 1
+N_ITER = 100000
+BATCH_SIZE = 16
+LR = 2e-5
+L2 = 1e-4
+
+
+class MCTS:
+    def __init__(self, n_block, channel, board_size, n_history, n_simul):
+        self.env_simul = OmokEnv(board_size, n_history, display=False)
+        self.model = PVNet(n_block, n_history*2+1, channel, board_size)
+        self.board_size = board_size
+        self.n_simul = n_simul
+        self.tree = None
+        self.root = None
+        self.root_key = None
+        self.state = None
+        self.board = None
+        # used for backup
+        self.key_memory = deque()
+        self.action_memory = deque()
+        self.reset_tree()
+
+    def reset_tree(self):
+        self.tree = defaultdict(lambda: zeros((self.board_size**2, 3)))
+
+    def get_action(self, state, board, tau):
+        self.root = state.copy()
+        self.root_key = hash(self.root.tostring())
+        self._simulation(state)
+        # init root board after simulatons
+        self.board = board
+        # argmax visit count
+        action, pi = self._selection(self.root_key, c_pucb=0)
+        if tau == 1:
+            action = random.choice(self.board_size**2, p=pi)
+
+        return action, pi
+
+    def _simulation(self, state):
+        start = time.time()
+        finish = 0
+        for sim in range(self.n_simul):
+            print('\rsimulation: {}'.format(sim + 1), end='')
+            sys.stdout.flush()
+            # reset state
+            self.state, self.board = self.env_simul.reset(state)
+            done = False
+
+            while not done:
+                key = hash(self.state.tostring())
+                # search my tree
+                if key in self.tree:
+                    # selection
+                    action = self._selection(key, c_pucb=5)
+                    self.action_memory.appendleft(action)
+                    self.key_memory.appendleft(key)
+                else:
+                    # expansion
+                    reward, done = self._expansion(key, self.state)
+                    break
+
+                self.state, self.board, reward, done = self.env_simul.step(
+                    action)
+
+            if done:
+                # backup & reset memory
+                self._backup(reward)
+                finish = time.time() - start
+                # if finish >= self.think_time:
+                #     break
+        print('\r{} simulations end ({:0.0f}s)'.format(sim + 1, finish))
+
+    def _selection(self, key, c_pucb):
+        edges = self.tree[key]
+        pucb = self._get_pucb(edges, key, c_pucb)
+
+        if c_pucb == 0:
+            visit = edges[:, N]
+            print('\nvisit count')
+            print(visit.reshape(self.board_size, self.board_size).round())
+            action = argwhere(visit == visit.max()).flatten()
+            action = action[random.choice(len(action))]
+            pi = exp(visit) / exp(visit).sum()
+            pi = visit / visit.sum()
+            print('\npi')
+            print(pi.reshape(
+                self.board_size, self.board_size).round(decimals=2))
+            return action, pi
+
+        if self.board[COLOR][0] == WHITE:
+            # black's choice
+            action = argwhere(pucb == pucb.max()).flatten()
+        else:
+            # white's choice
+            action = argwhere(pucb == pucb.min()).flatten()
+        action = action[random.choice(len(action))]
+        return action
+
+    def _expansion(self, key, state):
+        edges = self.tree[key]
+        state_input = Variable(Tensor([state.reshape(
+            HISTORY*2+1, self.board_size, self.board_size)]))
+        prior, value = self.model(state_input)
+        edges[:, P] = prior.data.cpu().numpy()[0]
+        done = True
+        return value.data.cpu().numpy()[0], done
+
+    def _backup(self, reward):
+        # update edges in my tree
+        while self.key_memory:
+            key = self.key_memory.popleft()
+            edges = self.tree[key]
+            if self.action_memory:
+                action = self.action_memory.popleft()
+                edges[action][N] += 1
+                edges[action][Q] += (reward - edges[action][Q]) / \
+                    edges[action][N]
+        return 0
+
+    def _get_no_legal_loc(self, board):
+        board_fill = board[CURRENT] + board[OPPONENT]
+        legal_action = argwhere(board_fill == 0).flatten()
+        return board_fill, legal_action
+
+    def _get_pucb(self, edges, key, c_pucb):
+        no_legal_loc, legal_action = self._get_no_legal_loc(self.board)
+        prob = edges[:, P]
+        if key == self.root_key:
+            noise = random.dirichlet(0.15 * ones(len(legal_action)))
+            for i, action in enumerate(legal_action):
+                prob[action] = noise[i]
+        total_N = edges.sum(0)[N]
+        # black's pucb
+        if self.board[COLOR][0] == WHITE:
+            no_legal_loc *= -999999
+            pucb = edges[:, Q] + \
+                c_pucb * prob * sqrt(total_N) / (edges[:, N] + 1) + \
+                no_legal_loc
+        # white's pucb
+        else:
+            no_legal_loc *= 999999
+            pucb = edges[:, Q] - \
+                c_pucb * prob * sqrt(total_N) / (edges[:, N] + 1) + \
+                no_legal_loc
+        return pucb
+
+
+LEN_GAME = 0
+
+
+def self_play(n_game):
+    global LEN_GAME
+    result = {'Black': 0, 'White': 0, 'Draw': 0}
+    for g in range(n_game):
+        print('#' * (BOARD_SIZE - 4),
+              ' GAME: {} '.format(g + 1),
+              '#' * (BOARD_SIZE - 4))
+        # reset state
+        samples = []
+        state, board = Env.reset()
+        done = False
+        move = 0
+        while not done:
+            LEN_GAME += 1
+            Env.render()
+            if move < 8:
+                tau = 1
+            else:
+                tau = 0
+            # start simulations
+            action, pi = Agent.get_action(state, board, tau)
+            # print state evaluation
+            state = state.reshape(HISTORY*2+1, BOARD_SIZE, BOARD_SIZE)
+            state_input = Variable(Tensor([state]))
+            prob, value = Agent.model(state_input)
+            print('\nprob')
+            print(
+                prob.data.cpu().numpy()[0].reshape(
+                    BOARD_SIZE, BOARD_SIZE).round(decimals=2))
+            print('\nvalue')
+            print(value.data.cpu().numpy()[0].round(decimals=4))
+            # collect samples
+            samples.append((state, pi))
+            state, board, z, done = Env.step(action)
+            move += 1
+        if done:
+            if z == 1:
+                result['Black'] += 1
+            elif z == -1:
+                result['White'] += 1
+            else:
+                result['Draw'] += 1
+
+            for i in range(len(samples)):
+                Memory.appendleft(
+                    (samples[i][0], samples[i][1], z))
+            # render & reset tree
+            Env.render()
+            Agent.reset_tree()
+        # result
+        print('')
+        print('=' * 20, " {}  Game End  ".format(g + 1), '=' * 20)
+        blw, whw, drw = result['Black'], result['White'], result['Draw']
+        stats = (
+            'Black Win: {}  White Win: {}  Draw: {}  Winrate: {:.2f}%'.format(
+                blw, whw, drw, blw/(blw+whw)*100 if blw+whw != 0 else 0))
+        print(stats, '\n')
+
+
+STEPS = 0
+
+
+def train(n_epoch):
+    global STEPS
+    print('memory size:', len(Memory))
+    print('learning rate:', LR)
+
+    dataloader = DataLoader(Memory,
+                            batch_size=BATCH_SIZE,
+                            shuffle=True,
+                            drop_last=True,
+                            pin_memory=use_cuda)
+
+    optimizer = optim.SGD(Agent.model.parameters(),
+                          lr=LR,
+                          momentum=0.9,
+                          weight_decay=L2)
+
+    for epoch in range(n_epoch):
+        running_loss = 0.
+
+        for i, (s, pi, z) in enumerate(dataloader):
+            if use_cuda:
+                s_batch = Variable(s.float()).cuda()
+                pi_batch = Variable(pi.float()).cuda()
+                z_batch = Variable(z.float()).cuda()
+            else:
+                s_batch = Variable(s.float())
+                pi_batch = Variable(pi.float())
+                z_batch = Variable(z.float())
+
+            p_batch, v_batch = Agent.model(s_batch)
+
+            loss = F.mse_loss(v_batch, z_batch) + \
+                torch.mean(torch.sum(-pi_batch * torch.log(p_batch), 1))
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.data[0]
+            STEPS += 1
+
+            print('{:3} step loss: {:.3f}'.format(
+                STEPS, running_loss / (i + 1)))
+
+
+if __name__ == '__main__':
+    np.set_printoptions(suppress=True)
+    np.random.seed(0)
+    torch.manual_seed(0)
+    torch.cuda.manual_seed_all(0)
+    use_cuda = torch.cuda.is_available()
+    print('cuda:', use_cuda)
+    Tensor = torch.cuda.FloatTensor if use_cuda else torch.FloatTensor
+    Memory = deque(maxlen=5120)
+    Env = OmokEnv(BOARD_SIZE, HISTORY)
+    Agent = MCTS(N_BLOCK, CHANNEL, BOARD_SIZE, HISTORY, N_SIMUL)
+
+    if use_cuda:
+        Agent.model.cuda()
+
+    for i in range(N_ITER):
+        self_play(N_GAME)
+
+        if LEN_GAME == 5120:
+            train(N_EPOCH)
+            torch.save(
+                Agent.model.state_dict(),
+                '{}_step_model.pickle'.format(STEPS))
+            LEN_GAME = 0
